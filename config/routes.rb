e5a0915009895645845745b876ef0fecb0898d7c
Lee Lawlor <primemod3@gmail.com>
2015-01-03 16:23:19 -0500

    add comments to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  # devise routes
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
+  # homepage
   root 'pages#home'
 
 end
